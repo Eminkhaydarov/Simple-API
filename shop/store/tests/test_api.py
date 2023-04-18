ec4a1509257f9bb330d3305852cac4c42b702bec
Eldar Minkhaydarov <e.minkhaydarov@gmail.com>
2023-04-18 12:33:33 +0500

    Add filter, search, order

--- shop/store/tests/test_api.py
+++ shop/store/tests/test_api.py
@@ -7,11 +7,39 @@ from store.serializers import ProductSerializer
 
 
 class ProductApiTestCase(APITestCase):
+    def setUp(self) -> None:
+        self.product_1 = Product.objects.create(name='book_1', price=26)
+        self.product_2 = Product.objects.create(name='book_2', price=27)
+        self.product_3 = Product.objects.create(name='book 27.00', price=26)
+
     def test_get(self):
-        product_1 = Product.objects.create(name='book_1', price=26)
-        product_2 = Product.objects.create(name='book_2', price=27)
         url = reverse('product-list')
-        serializer_data = ProductSerializer([product_1, product_2], many=True).data
+        serializer_data = ProductSerializer([self.product_1, self.product_2, self.product_3], many=True).data
         response = self.client.get(url)
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(serializer_data, response.data)
+
+    def test_get_filter(self):
+        url = reverse('product-list')
+        serializer_data = ProductSerializer([self.product_1, self.product_3], many=True).data
+        response = self.client.get(url, data={'price': '26.00'})
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(serializer_data, response.data)
+
+    def test_get_search(self):
+        url = reverse('product-list')
+        serializer_data = ProductSerializer([self.product_2, self.product_3], many=True).data
+        response = self.client.get(url, data={'search': '27.00'})
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(serializer_data, response.data)
+
+    def test_get_order(self):
+        url = reverse('product-list')
+        serializer_data = ProductSerializer([self.product_1, self.product_3, self.product_2,], many=True).data
+        response = self.client.get(url, data={'ordering': 'price'})
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(serializer_data, response.data)
+        response = self.client.get(url, data={'ordering': '-price'})
+        serializer_data = ProductSerializer([self.product_2, self.product_1, self.product_3,], many=True).data
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data)
--- shop/store/views.py
+++ shop/store/views.py
@@ -1,3 +1,5 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet
 
 from store.models import Product
@@ -7,4 +9,7 @@ from store.serializers import ProductSerializer
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['price']
+    search_fields = ['name', 'price']
+    ordering_field = ['name', 'price']
